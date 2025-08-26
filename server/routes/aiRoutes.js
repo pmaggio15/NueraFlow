89204778ea4180591ae537e76f815a53caf90034
Your Name <you@example.com>
2025-08-26 16:01:54 -0600

    backend

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { generateArticle } from '../controllers/aiController.js';
+import { generateArticle, generateBlogTitle, generateImage, changePlan } from '../controllers/aiController.js';
 import { auth } from '../middlewares/auth.js';
 
 const aiRouter = express.Router();
 
 aiRouter.post('/generate-article', auth, generateArticle)
+aiRouter.post('/generate-blog-title', auth, generateBlogTitle)
+aiRouter.post('/generate-image', auth, generateImage)
+aiRouter.post('/change-plan', auth, changePlan)
 
 export default aiRouter
